5dd9a6d333dfd4b8c3cd54935890566561184f4b
lajarre <alexandre.hajjar@gmail.com>
2022-10-26 09:57:39 +0100

    ✨ Implement deposit and exchange

--- src/MoltenFundraiser.sol
+++ src/MoltenFundraiser.sol
@@ -1,34 +1,80 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MoltenFundraiser {
+contract MoltenFundraiser is ERC20 {
     uint256 public lockingDuration;
     uint256 public exchangeTime;
-    IERC20 public daoToken;
+    ERC20 public daoToken;
 
     IERC20 public depositToken;
-    uint256 public totalDeposits;
-    mapping(address => uint256) public deposits;
+    // ⚠️  This mapping is not emptied on exchange. After exchange, its values
+    // are really only what was deposited by a given address.
+    mapping(address => uint256) public deposited;
+    // ⚠️  Not emptied on exchange.
+    uint256 public totalDeposited;
+    address public daoTreasuryAddress;
+
+    uint256 public exchangeRate;
 
     constructor(
         address daoTokenAddress,
         uint256 _lockingDuration,
-        address depositTokenAddress
-    ) public {
+        address depositTokenAddress,
+        address _daoTreasuryAddress
+    )
+        public
+        ERC20("Molten token", "mToken") // ERC20(
+    //     string.concat("Molten ", daoToken.name()),
+    //     string.concat("m", daoToken.symbol())
+    // )
+    {
         lockingDuration = _lockingDuration;
-        daoToken = IERC20(daoTokenAddress);
+        daoToken = ERC20(daoTokenAddress);
         depositToken = IERC20(depositTokenAddress);
+        daoTreasuryAddress = _daoTreasuryAddress;
     }
 
     function deposit(uint256 amount) public {
-        deposits[msg.sender] += amount;
-        totalDeposits += amount;
+        require(exchangeTime == 0, "Molten: exchange happened");
+        deposited[msg.sender] += amount;
+        totalDeposited += amount;
         depositToken.transferFrom(msg.sender, address(this), amount);
     }
 
-    function exchange(uint256 amount) public {
-        daoToken.transferFrom(msg.sender, address(this), amount);
+    /**
+     * @param _exchangeRate is the number of deposit wei-tokens valued the same as 1
+     * DAO token.
+     */
+    function exchange(uint256 _exchangeRate) public {
+        require(exchangeTime == 0, "Molten: exchange happened");
+        exchangeTime = block.timestamp;
+
+        exchangeRate = _exchangeRate;
+        uint256 daoTokenTotal = totalDeposited / exchangeRate;
+
+        _mint(address(this), daoTokenTotal);
+        depositToken.transfer(daoTreasuryAddress, totalDeposited);
+        daoToken.transferFrom(msg.sender, address(this), daoTokenTotal);
     }
+
+    function claimMTokens() public {
+        require(exchangeTime > 0, "Molten: exchange not happened");
+
+        // [FIXME] We are not making sure that the total amount of claimable
+        // mTokens is going to match exactly the total minted supply.
+        _transfer(
+            address(this),
+            msg.sender,
+            deposited[msg.sender] / exchangeRate
+        );
+    }
+
+    // function redeem() public {
+    //     require(
+    //         block.timestamp >= exchangeTime + lockingDuration,
+    //         "Molten: still locked"
+    //     );
+    // }
 }
--- test/MoltenFundraiser.t.sol
+++ test/MoltenFundraiser.t.sol
@@ -5,7 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {MoltenFundraiser} from "../src/MoltenFundraiser.sol";
 import {ERC20PresetMinterPauser} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract MoltenFundraiserTest is Test {
+abstract contract MoltenFundraiserTestBase is Test {
     ERC20PresetMinterPauser public daoToken;
     MoltenFundraiser public moltenFundraiser;
     address public daoTreasuryAddress = address(0x1);
@@ -13,52 +13,154 @@ contract MoltenFundraiserTest is Test {
     ERC20PresetMinterPauser public depositToken;
     address public depositorAddress = address(0x2);
 
-    function setUp() public {
+    function setUp() public virtual {
         daoToken = new ERC20PresetMinterPauser("DAO governance token", "GT");
         depositToken = new ERC20PresetMinterPauser("Stable token", "BAI");
         moltenFundraiser = new MoltenFundraiser(
             address(daoToken),
             365 days,
-            address(depositToken)
+            address(depositToken),
+            daoTreasuryAddress
         );
         vm.label(daoTreasuryAddress, "DAO treasury");
+        vm.label(depositorAddress, "Depositor");
+        vm.label(address(moltenFundraiser), "Molten fundraiser");
     }
+}
 
+contract MoltenFundraiserTest is MoltenFundraiserTestBase {
     function testConstructor() public {
         assertEq(moltenFundraiser.lockingDuration(), 365 days);
     }
 
-    function testExchangeMissingFunds() public {
-        vm.expectRevert(bytes("ERC20: insufficient allowance"));
-        vm.prank(daoTreasuryAddress);
-        moltenFundraiser.exchange(1);
-    }
-
-    function testExchangeGTReceivedByFundraiser() public {
-        daoToken.mint(daoTreasuryAddress, 1000);
-        vm.prank(daoTreasuryAddress);
-        daoToken.approve(address(moltenFundraiser), 1000);
-
-        vm.prank(daoTreasuryAddress);
-        moltenFundraiser.exchange(1000);
-
-        assertEq(daoToken.balanceOf(address(moltenFundraiser)), 1000);
-    }
-
     function testDepositMissingFunds() public {
-        depositToken.mint(depositorAddress, 1000);
+        depositToken.mint(depositorAddress, 1000 * 10**18);
 
         vm.expectRevert();
         vm.prank(depositorAddress);
+        moltenFundraiser.deposit(1000 * 10**18);
+    }
+
+    function testExchangeMissingFunds() public {
+        depositToken.mint(depositorAddress, 1000 * 10**18);
+        vm.prank(depositorAddress);
+        depositToken.approve(address(moltenFundraiser), 1000 * 10**18);
+        vm.prank(depositorAddress);
+        moltenFundraiser.deposit(1000 * 10**18);
+
+        vm.expectRevert(bytes("ERC20: insufficient allowance"));
+        vm.prank(daoTreasuryAddress);
+        moltenFundraiser.exchange(20);
+    }
+
+    function testClaimBlocked() public {
+        vm.expectRevert("Molten: exchange not happened");
+        vm.prank(depositorAddress);
+        moltenFundraiser.claimMTokens();
+    }
+}
+
+contract MoltenFundraiserDepositTest is MoltenFundraiserTestBase {
+    function setUp() public override {
+        super.setUp();
+
+        depositToken.mint(depositorAddress, 1000 * 10**18);
+        vm.prank(depositorAddress);
+        depositToken.approve(address(moltenFundraiser), 1000 * 10**18);
+
+        vm.prank(depositorAddress);
+        moltenFundraiser.deposit(1000 * 10**18);
+    }
+
+    function testRecordsDeposit() public {
+        assertEq(moltenFundraiser.deposited(depositorAddress), 1000 * 10**18);
+    }
+
+    function testIncreasesTotalDeposits() public {
+        depositToken.mint(address(0x234), 234 * 10**18);
+        vm.prank(address(0x234));
+        depositToken.approve(address(moltenFundraiser), 234 * 10**18);
+        vm.prank(address(0x234));
+        moltenFundraiser.deposit(234 * 10**18);
+
+        assertEq(moltenFundraiser.totalDeposited(), 1234 * 10**18);
+    }
+}
+
+abstract contract MoltenFundraiserExchangeTestBase is MoltenFundraiserTestBase {
+    uint256 public initialDaoTreasuryDepositBalance;
+    uint256 public initialTotalDesposits;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        daoToken.mint(daoTreasuryAddress, 4242 * 10**18);
+        vm.prank(daoTreasuryAddress);
+        daoToken.approve(address(moltenFundraiser), type(uint256).max);
+
+        depositToken.mint(depositorAddress, 1000 * 10**18);
+        vm.prank(depositorAddress);
+        depositToken.approve(address(moltenFundraiser), 1000 * 10**18);
+
+        initialDaoTreasuryDepositBalance = depositToken.balanceOf(
+            daoTreasuryAddress
+        );
+
+        vm.prank(depositorAddress);
+        moltenFundraiser.deposit(1000 * 10**18);
+        vm.prank(daoTreasuryAddress);
+        moltenFundraiser.exchange(20);
+    }
+}
+
+contract MoltenFundraiserExchangeTest is MoltenFundraiserExchangeTestBase {
+    function testSetExchangeTime() public {
+        assert(moltenFundraiser.exchangeTime() > 0);
+    }
+
+    function testTransfersDaoTokensToFundraiser() public {
+        assertEq(daoToken.balanceOf(address(moltenFundraiser)), 50 * 10**18);
+    }
+
+    function testTransfersDepositsToDaoTreasury() public {
+        assertEq(depositToken.balanceOf(address(moltenFundraiser)), 0);
+        assertEq(
+            depositToken.balanceOf(daoTreasuryAddress) -
+                initialDaoTreasuryDepositBalance,
+            1000 * 10**18
+        );
+    }
+
+    function testRepeatedDepositFails() public {
+        vm.expectRevert("Molten: exchange happened");
+        vm.prank(depositorAddress);
         moltenFundraiser.deposit(1000);
     }
 
-    function testDeposit() public {
-        depositToken.mint(depositorAddress, 1000);
-        vm.prank(depositorAddress);
-        depositToken.approve(address(moltenFundraiser), 1000);
+    function testRepeatedExchangeFails() public {
+        vm.expectRevert("Molten: exchange happened");
+        vm.prank(daoTreasuryAddress);
+        moltenFundraiser.exchange(20);
+    }
+
+    function testMintsMTokens() public {
+        assertEq(moltenFundraiser.totalSupply(), (1000 / 20) * 10**18);
+    }
+}
+
+contract MoltenFundraiserClaimTest is MoltenFundraiserExchangeTestBase {
+    function setUp() public override {
+        super.setUp();
 
         vm.prank(depositorAddress);
-        moltenFundraiser.deposit(1000);
+        moltenFundraiser.claimMTokens();
+    }
+
+    function testTransfersToDepositor() public {
+        assertEq(moltenFundraiser.balanceOf(address(moltenFundraiser)), 0);
+        assertEq(
+            moltenFundraiser.balanceOf(depositorAddress),
+            (1000 / 20) * 10**18
+        );
     }
 }
