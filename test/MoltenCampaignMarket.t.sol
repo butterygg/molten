de2120c7d4cd7017b36d66402147bd9d511f58c9
lajarre <alexandre.hajjar@gmail.com>
2023-01-15 15:15:51 +0000

    [wip] ✨ Molten pots

---
+++ test/MoltenCampaignMarket.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import {ERC20VotesMintable} from "./helpers/ERC20VotesMintable.sol";
+import {MoltenCampaignMarket} from "../src/MoltenCampaign.sol";
+
+contract CreationTest is Test {
+    ERC20VotesMintable daoToken;
+
+    function setUp() public {
+        daoToken = new ERC20VotesMintable("DAO governance token", "GT");
+        vm.label(address(daoToken), "DAO governance token");
+    }
+
+    function testHasDaoToken() public {
+        MoltenCampaignMarket mcm = new MoltenCampaignMarket(address(daoToken), 1);
+
+        assertEq(address(mcm.daoToken()), address(daoToken));
+    }
+
+    function testHasThreshold(uint256 threshold) public {
+        MoltenCampaignMarket mcm = new MoltenCampaignMarket(address(daoToken), threshold);
+
+        assertEq(mcm.threshold(), threshold);
+    }
+}
