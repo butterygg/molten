65bd3195c3085929e1aea1608f9d8104ab7baeb6
lajarre <alexandre.hajjar@gmail.com>
2023-02-07 23:19:45 +0000

    🎨 Rename things

--- test/MoltenCampaign.t.sol
+++ test/MoltenCampaign.t.sol
@@ -2,41 +2,43 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import {MoltenCampaign, MoltenCampaignMarket} from "../src/MoltenCampaign.sol";
+import {MoltenCampaign, MoltenElection} from "../src/MoltenCampaign.sol";
 import {MToken} from "../src/MToken.sol";
 import {ERC20VotesMintable} from "./helpers/ERC20VotesMintable.sol";
 import {ERC20VotesMintableMock} from "./helpers/ERC20VotesMintable.sol";
 import {MTokenMock} from "./helpers/MTokenMock.sol";
 
 abstract contract TestBase is Test {
-    uint256 public threshold;
-    uint128 public duration;
-    uint128 public cooldownDuration;
-    address public representative = address(0x123);
-    MoltenCampaign public mc;
+    uint256 public threshold = 1;
+    uint128 public duration = 1;
+    uint128 public cooldownDuration = 1;
+    address public delegate = address(0x123);
+    string public delegateName = "deldel";
+    MoltenCampaign public campaign;
 
     ERC20VotesMintableMock public daoToken;
     MTokenMock public mToken;
 
     function setUp() public virtual {
         daoToken = new ERC20VotesMintableMock("DAO governance token", "GT");
-        threshold = 1;
-        duration = 1;
-        cooldownDuration = 1;
-        MoltenCampaignMarket mcm = new MoltenCampaignMarket(
+        MoltenElection election = new MoltenElection(
             address(daoToken),
             threshold,
             duration,
             cooldownDuration
         );
-        vm.prank(representative);
+        vm.prank(delegate);
         mToken = new MTokenMock(
             string.concat("Molten ", daoToken.name()),
-            string.concat("m", daoToken.symbol()), // [XXX] Add campaigner (delegate) name
+            string.concat("m", daoToken.symbol(), "-", delegateName),
             address(this)
         );
-        mc = new MoltenCampaign(representative, address(mcm), address(mToken));
-        mToken.transferOwnership(address(mc));
+        campaign = new MoltenCampaign(
+            delegate,
+            address(election),
+            address(mToken)
+        );
+        mToken.transferOwnership(address(campaign));
     }
 
     function setFailTransfer() public virtual {
@@ -57,48 +59,36 @@ abstract contract TestBase is Test {
 }
 
 contract StakeTest is TestBase {
-    address public staker;
-    address public staker2;
+    address public staker = address(0x331);
+    address public staker2 = address(0x332);
 
     function setUp() public override {
         super.setUp();
 
-        staker = address(0x331);
-        staker2 = address(0x332);
+        vm.prank(staker);
+        campaign.stake(333);
     }
 
     function testStakeUpdatesStaked() public {
-        vm.prank(staker);
-        mc.stake(333);
-
-        assertEq(mc.staked(staker), 333);
+        assertEq(campaign.staked(staker), 333);
     }
 
     function testStakeUpdatesTotalStaked() public {
-        vm.prank(staker);
-        mc.stake(333);
-
         vm.prank(staker2);
-        mc.stake(222);
+        campaign.stake(222);
 
-        assertEq(mc.totalStaked(), 555);
+        assertEq(campaign.totalStaked(), 555);
     }
 
     function testStakeCallsTransfer() public {
-        vm.prank(staker);
-        mc.stake(333);
-
         (address from, address to, uint256 amount) = daoToken
             .transferFromCalledWith();
         assertEq(from, staker);
-        assertEq(to, address(mc));
+        assertEq(to, address(campaign));
         assertEq(amount, 333);
     }
 
     function testStakeMintsMTokens() public {
-        vm.prank(staker);
-        mc.stake(333);
-
         (address _sender, address to, uint256 amount) = mToken.mintCalledWith();
         assertEq(_sender, mToken.owner());
         assertEq(to, staker);
@@ -106,10 +96,7 @@ contract StakeTest is TestBase {
     }
 
     function testStakeResetsCooldown() public {
-        vm.prank(staker);
-        mc.stake(333);
-
-        assertEq(mc.cooldownEnd(), block.timestamp + cooldownDuration);
+        assertEq(campaign.cooldownEnd(), block.timestamp + cooldownDuration);
     }
 }
 
@@ -125,7 +112,7 @@ contract CantTransferStakeTest is TestBase {
     function testWrongStakeReverts() public {
         vm.prank(staker);
         vm.expectRevert("ERC20VMFM transferFrom");
-        mc.stake(333);
+        campaign.stake(333);
     }
 }
 
@@ -141,7 +128,7 @@ contract CantMintStakeTest is TestBase {
     function testWrongStakeReverts() public {
         vm.prank(staker);
         vm.expectRevert("MTFM mint");
-        mc.stake(333);
+        campaign.stake(333);
     }
 }
 
@@ -155,42 +142,33 @@ contract UnstakeTest is TestBase {
         // We call stake as a means to update the `totalStaked` and `staked`
         // states.
         vm.prank(staker);
-        mc.stake(333);
+        campaign.stake(333);
         vm.prank(staker2);
-        mc.stake(222);
+        campaign.stake(222);
+        vm.prank(staker);
+        campaign.unstake();
     }
 
     function testUnstakeUpdatesStaked() public {
-        vm.prank(staker);
-        mc.unstake();
-        assertEq(mc.staked(staker), 0);
+        assertEq(campaign.staked(staker), 0);
     }
 
     function testUnstakeUpdatesTotalStaked() public {
-        vm.prank(staker);
-        mc.unstake();
-
         vm.prank(staker2);
-        mc.unstake();
+        campaign.unstake();
 
-        assertEq(mc.totalStaked(), 0);
+        assertEq(campaign.totalStaked(), 0);
     }
 
     function testUnstakeCallsTransfer() public {
-        vm.prank(staker);
-        mc.unstake();
-
         (address from, address to, uint256 amount) = daoToken
             .transferFromCalledWith();
-        assertEq(from, address(mc));
+        assertEq(from, address(campaign));
         assertEq(to, staker);
         assertEq(amount, 333);
     }
 
     function testUnstakeBurnsMTokens() public {
-        vm.prank(staker);
-        mc.unstake();
-
         (address _sender, address to, uint256 amount) = mToken.burnCalledWith();
         assertEq(_sender, mToken.owner());
         assertEq(to, staker);
@@ -198,24 +176,17 @@ contract UnstakeTest is TestBase {
     }
 
     function testUnstakeResetsCooldown() public {
-        vm.prank(staker);
-        mc.unstake();
-
-        assertEq(mc.cooldownEnd(), block.timestamp + cooldownDuration);
+        assertEq(campaign.cooldownEnd(), block.timestamp + cooldownDuration);
     }
 }
 
 contract UnstakeNoStakeTest is TestBase {
     address public staker = address(0x331);
 
-    function setUp() public override {
-        super.setUp();
-    }
-
     function testUnstakeReverts() public {
         vm.prank(staker);
         vm.expectRevert("Molten: unstake 0");
-        mc.unstake();
+        campaign.unstake();
     }
 }
 
@@ -226,14 +197,14 @@ contract CantTransferUnstakeTest is TestBase {
         super.setUp();
 
         vm.prank(staker);
-        mc.stake(333);
+        campaign.stake(333);
         setFailTransfer();
     }
 
     function testWrongUnstakeReverts() public {
         vm.prank(staker);
         vm.expectRevert("ERC20VMFM transferFrom");
-        mc.unstake();
+        campaign.unstake();
     }
 }
 
@@ -243,16 +214,15 @@ contract CantBurnUnstakeTest is TestBase {
     function setUp() public override {
         super.setUp();
 
-        unsetFailMint();
         staker = address(0x331);
         vm.prank(staker);
-        mc.stake(333); // Set up MoltenCampaign state.
+        campaign.stake(333); // Set up MoltenCampaign state.
         setFailMint();
     }
 
     function testWrongUntakeReverts() public {
         vm.prank(staker);
         vm.expectRevert("MTFM burn");
-        mc.unstake();
+        campaign.unstake();
     }
 }
