873467ff4d56269d40f8e9b9fc56b220a964b5dc
lajarre <alexandre.hajjar@gmail.com>
2023-02-12 23:14:14 +0000

    ✅ Make tests more robust and simple, use factories

--- test/MoltenCampaign.t.sol
+++ test/MoltenCampaign.t.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import {MoltenCampaign, MoltenElection} from "../src/MoltenCampaign.sol";
+import {MoltenCampaign} from "../src/MoltenCampaign.sol";
 import {MToken} from "../src/MToken.sol";
 import {ERC20VotesMintable} from "./helpers/ERC20VotesMintable.sol";
 import {ERC20VotesMintableMock} from "./helpers/ERC20VotesMintable.sol";
 import {MTokenMock} from "./helpers/MTokenMock.sol";
-import {MoltenElectionMock} from "./helpers/MoltenCampaignMock.sol";
+import {MoltenElectionStub, MoltenElectionMock, MoltenCampaignStub} from "./helpers/MoltenCampaignMock.sol";
 
 abstract contract TestBase is Test {
     uint256 public threshold = 100;
@@ -15,14 +15,16 @@ abstract contract TestBase is Test {
     uint128 public cooldownDuration = 15;
     address public delegate = address(0x123);
     string public delegateName = "deldel";
-    MoltenCampaign public campaign;
+    MoltenCampaignStub public campaign;
 
     ERC20VotesMintableMock public daoToken;
     MTokenMock public mToken;
 
+    // See MoltenCampaignFactory
     function setUp() public virtual {
         daoToken = new ERC20VotesMintableMock("DAO governance token", "GT");
-        MoltenElection election = new MoltenElection(
+        MoltenElectionStub election = new MoltenElectionStub(
+            address(0xdeadbeefcf),
             address(daoToken),
             threshold,
             duration,
@@ -34,12 +36,13 @@ abstract contract TestBase is Test {
             string.concat("m", daoToken.symbol(), "-", delegateName),
             address(this)
         );
-        campaign = new MoltenCampaign(
+        campaign = new MoltenCampaignStub(
             delegate,
             address(election),
             address(mToken)
         );
         mToken.transferOwnership(address(campaign));
+        election.__stubHasCampaign(address(campaign), true);
     }
 
     function setFailTransfer() public virtual {
@@ -56,6 +59,58 @@ abstract contract TestBase is Test {
 
     function unsetFailMint() public virtual {
         mToken.__unsetFail();
+    }
+}
+
+contract CreationTest is Test {
+    MoltenCampaign public campaign;
+
+    function setUp() public {
+        campaign = new MoltenCampaign(
+            address(0x4242),
+            address(0x42424242),
+            address(0x424242424242)
+        );
+    }
+
+    function testSetsRepresentative() public {
+        assertEq(campaign.representative(), address(0x4242));
+    }
+
+    function testSetsElection() public {
+        assertEq(address(campaign.election()), address(0x42424242));
+    }
+
+    function testSetsMToken() public {
+        assertEq(address(campaign.mToken()), address(0x424242424242));
+    }
+}
+
+contract CantStakeTest is TestBase {
+    address public staker = address(0x331);
+
+    function testNeedsSuccessfulMint() public {
+        setFailMint();
+
+        vm.prank(staker);
+        vm.expectRevert("MTFM mint");
+        campaign.stake(333);
+    }
+
+    function testNeedsSuccessfulTransfer() public {
+        setFailTransfer();
+
+        vm.prank(staker);
+        vm.expectRevert("ERC20VMFM transferFrom");
+        campaign.stake(333);
+    }
+
+    function testRequiresNotInOffice() public {
+        campaign.__stubInOffice(true);
+
+        vm.prank(staker);
+        vm.expectRevert("Molten: in office");
+        campaign.stake(333);
     }
 }
 
@@ -102,35 +157,43 @@ contract StakeTest is TestBase {
     }
 }
 
-contract CantTransferStakeTest is TestBase {
+contract CantUnstakeTest is TestBase {
     address public staker = address(0x331);
 
-    function setUp() public override {
-        super.setUp();
-
-        setFailTransfer();
+    function testNeedsAStake() public {
+        vm.prank(staker);
+        vm.expectRevert("Molten: unstake 0");
+        campaign.unstake();
     }
 
-    function testWrongStakeReverts() public {
+    function testNeedsSucessfulTransfer() public {
+        campaign.__stubStaked(staker, 333);
+        campaign.__stubTotalStaked(333);
+        setFailTransfer();
+
         vm.prank(staker);
         vm.expectRevert("ERC20VMFM transferFrom");
-        campaign.stake(333);
+        campaign.unstake();
     }
-}
 
-contract CantMintStakeTest is TestBase {
-    address public staker = address(0x331);
-
-    function setUp() public override {
-        super.setUp();
-
+    function testNeedsNeedsSuccessfulBurn() public {
+        campaign.__stubStaked(staker, 333);
+        campaign.__stubTotalStaked(333);
         setFailMint();
+
+        vm.prank(staker);
+        vm.expectRevert("MTFM burn");
+        campaign.unstake();
     }
 
-    function testWrongStakeReverts() public {
+    function testRequiresNotInOffice() public {
+        campaign.__stubStaked(staker, 333);
+        campaign.__stubTotalStaked(333);
+        campaign.__stubInOffice(true);
+
         vm.prank(staker);
-        vm.expectRevert("MTFM mint");
-        campaign.stake(333);
+        vm.expectRevert("Molten: in office");
+        campaign.unstake();
     }
 }
 
@@ -141,21 +204,21 @@ contract UnstakeTest is TestBase {
     function setUp() public override {
         super.setUp();
 
-        // We call stake as a means to update the `totalStaked` and `staked`
-        // states.
-        vm.prank(staker);
-        campaign.stake(333);
-        vm.prank(staker2);
-        campaign.stake(222);
-        vm.prank(staker);
-        campaign.unstake();
+        campaign.__stubStaked(staker, 333);
+        campaign.__stubStaked(staker2, 222);
+        campaign.__stubTotalStaked(555);
     }
 
     function testUnstakeUpdatesStaked() public {
+        vm.prank(staker);
+        campaign.unstake();
+
         assertEq(campaign.staked(staker), 0);
     }
 
     function testUnstakeUpdatesTotalStaked() public {
+        vm.prank(staker);
+        campaign.unstake();
         vm.prank(staker2);
         campaign.unstake();
 
@@ -163,6 +226,9 @@ contract UnstakeTest is TestBase {
     }
 
     function testUnstakeCallsTransfer() public {
+        vm.prank(staker);
+        campaign.unstake();
+
         (address from, address to, uint256 amount) = daoToken
             .__transferFromCalledWith();
         assertEq(from, address(campaign));
@@ -171,6 +237,9 @@ contract UnstakeTest is TestBase {
     }
 
     function testUnstakeBurnsMTokens() public {
+        vm.prank(staker);
+        campaign.unstake();
+
         (address _sender, address to, uint256 amount) = mToken
             .__burnCalledWith();
         assertEq(_sender, mToken.owner());
@@ -179,70 +248,34 @@ contract UnstakeTest is TestBase {
     }
 
     function testUnstakeResetsCooldown() public {
+        vm.prank(staker);
+        campaign.unstake();
+
         assertEq(campaign.cooldownEnd(), block.timestamp + cooldownDuration);
-    }
-}
-
-contract UnstakeNoStakeTest is TestBase {
-    address public staker = address(0x331);
-
-    function testUnstakeReverts() public {
-        vm.prank(staker);
-        vm.expectRevert("Molten: unstake 0");
-        campaign.unstake();
-    }
-}
-
-contract CantTransferUnstakeTest is TestBase {
-    address public staker = address(0x331);
-
-    function setUp() public override {
-        super.setUp();
-
-        vm.prank(staker);
-        campaign.stake(333);
-        setFailTransfer();
-    }
-
-    function testWrongUnstakeReverts() public {
-        vm.prank(staker);
-        vm.expectRevert("ERC20VMFM transferFrom");
-        campaign.unstake();
-    }
-}
-
-contract CantBurnUnstakeTest is TestBase {
-    address public staker;
-
-    function setUp() public override {
-        super.setUp();
-
-        staker = address(0x331);
-        vm.prank(staker);
-        campaign.stake(333); // Set up MoltenCampaign state.
-        setFailMint();
-    }
-
-    function testWrongUntakeReverts() public {
-        vm.prank(staker);
-        vm.expectRevert("MTFM burn");
-        campaign.unstake();
     }
 }
 
 contract CantTakeOfficeTest is TestBase {
     address public staker = address(0x331);
 
-    function testNoTresholdCantTakeOffice() public {
+    function testNoTresholdCantTakeOffice(uint256 stake) public {
+        vm.assume(stake < threshold);
+        campaign.__stubStaked(staker, stake);
+        campaign.__stubTotalStaked(stake);
+
         vm.expectRevert("Molten: threshold not reached");
         campaign.takeOffice();
     }
 
-    function testCooldownNotEndedCantTakeOffice(uint256 stake) public {
+    function testCooldownNotEndedCantTakeOffice(
+        uint256 stake,
+        uint256 cooldownEnd
+    ) public {
         vm.assume(stake >= threshold);
-
-        vm.prank(staker);
-        campaign.stake(stake);
+        vm.assume(cooldownEnd > block.timestamp);
+        campaign.__stubStaked(staker, stake);
+        campaign.__stubTotalStaked(stake);
+        campaign.__stubCooldownEnd(cooldownEnd);
 
         vm.expectRevert("Molten: cooldown ongoing");
         campaign.takeOffice();
@@ -250,28 +283,26 @@ contract CantTakeOfficeTest is TestBase {
 
     function testTresholdAndCooldownOkCanTakeOffice(
         uint256 stake,
-        uint256 blockTimestamp
+        uint256 cooldownEnd
     ) public {
         vm.assume(stake >= threshold);
+        vm.assume(cooldownEnd <= block.timestamp);
+        campaign.__stubStaked(staker, stake);
+        campaign.__stubTotalStaked(stake);
+        campaign.__stubCooldownEnd(cooldownEnd);
 
-        vm.prank(staker);
-        campaign.stake(stake);
-
-        vm.assume(blockTimestamp >= campaign.cooldownEnd());
-
-        vm.warp(blockTimestamp);
         campaign.takeOffice();
     }
 }
 
 contract TakeOfficeTest is TestBase {
     address public staker = address(0x331);
-    address public staker2 = address(0x332);
-    MoltenElectionMock election;
+    MoltenElectionMock public election;
 
     function setUp() public override {
         daoToken = new ERC20VotesMintableMock("DAO governance token", "GT");
         election = new MoltenElectionMock(
+            address(0xdeadbeefcf),
             address(daoToken),
             threshold,
             duration,
@@ -283,33 +314,38 @@ contract TakeOfficeTest is TestBase {
             string.concat("m", daoToken.symbol(), "-", delegateName),
             address(this)
         );
-        campaign = new MoltenCampaign(
+        campaign = new MoltenCampaignStub(
             delegate,
             address(election),
             address(mToken)
         );
         mToken.transferOwnership(address(campaign));
+    }
 
-        vm.prank(staker);
-        campaign.stake(threshold);
-        vm.warp(campaign.cooldownEnd());
+    function testSetsInOffice(uint256 totalStaked, uint256 cooldownEnd) public {
+        vm.assume(totalStaked >= threshold);
+        vm.assume(cooldownEnd <= block.timestamp);
+        campaign.__stubStaked(staker, totalStaked);
+        campaign.__stubTotalStaked(totalStaked);
+        campaign.__stubCooldownEnd(cooldownEnd);
+
         campaign.takeOffice();
+
+        assertTrue(campaign.inOffice());
     }
 
-    function testCantStake() public {
-        vm.prank(staker);
-        vm.expectRevert("Molten: in office");
-        campaign.stake(1);
-    }
+    function testEndsElection(uint256 totalStaked, uint256 cooldownEnd) public {
+        vm.assume(totalStaked >= threshold);
+        vm.assume(cooldownEnd <= block.timestamp);
+        campaign.__stubStaked(staker, totalStaked);
+        campaign.__stubTotalStaked(totalStaked);
+        campaign.__stubCooldownEnd(cooldownEnd);
 
-    function testCantUnstake() public {
-        vm.prank(staker);
-        vm.expectRevert("Molten: in office");
-        campaign.unstake();
-    }
+        campaign.takeOffice();
 
-    function testEndsElection() public {
         address from = election.__endCalledWith();
+        emit log_address(from);
+        emit log_address(address(campaign));
         assertEq(from, address(campaign));
     }
 }
--- test/helpers/MoltenCampaignMock.sol
+++ test/helpers/MoltenCampaignMock.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 // solhint-disable not-rely-on-time
+// solhint-disable contract-name-camelcase
 pragma solidity ^0.8.17;
 
 import {MToken} from "../../src/MToken.sol";
-import {MoltenElection} from "../../src/MoltenCampaign.sol";
+import {MoltenElection, MoltenCampaign} from "../../src/MoltenCampaign.sol";
 import {IERC20Votes} from "../../src/interfaces/IERC20Votes.sol";
 
 contract MoltenElectionMock {
+    address public campaignFactory;
     IERC20Votes public daoToken;
     // Threshold in daoToken-weis.
     uint256 public threshold;
@@ -20,11 +22,13 @@ contract MoltenElectionMock {
     __EndCall public __endCalledWith;
 
     constructor(
+        address campaignFactoryAddress,
         address daoTokenAddress,
         uint256 _threshold,
         uint128 _duration,
         uint128 _cooldownDuration
     ) {
+        campaignFactory = campaignFactoryAddress;
         daoToken = IERC20Votes(daoTokenAddress);
         threshold = _threshold;
         duration = _duration;
@@ -33,6 +37,32 @@ contract MoltenElectionMock {
 
     function end() public {
         __endCalledWith = __EndCall(msg.sender);
+    }
+}
+
+contract MoltenElectionStub is MoltenElection {
+    constructor(
+        address campaignFactoryAddress,
+        address daoTokenAddress,
+        uint256 _threshold,
+        uint128 _duration,
+        uint128 _cooldownDuration
+    )
+        MoltenElection(
+            campaignFactoryAddress,
+            daoTokenAddress,
+            _threshold,
+            _duration,
+            _cooldownDuration
+        )
+    {} // solhint-disable no-empty-blocks
+
+    function __stubEnded(bool _ended) public {
+        ended = _ended;
+    }
+
+    function __stubHasCampaign(address campaign, bool hasIt) public {
+        hasCampaign[campaign] = hasIt;
     }
 }
 
@@ -64,5 +94,29 @@ contract MoltenCampaignMock {
 
     function __stubCooldownEnd(uint256 _cooldownEnd) public {
         cooldownEnd = _cooldownEnd;
+    }
+}
+
+contract MoltenCampaignStub is MoltenCampaign {
+    constructor(
+        address _representative,
+        address electionAddress,
+        address mTokenAddress
+    ) MoltenCampaign(_representative, electionAddress, mTokenAddress) {} // solhint-disable no-empty-blocks
+
+    function __stubTotalStaked(uint256 _totalStaked) public {
+        totalStaked = _totalStaked;
+    }
+
+    function __stubCooldownEnd(uint256 _cooldownEnd) public {
+        cooldownEnd = _cooldownEnd;
+    }
+
+    function __stubStaked(address voter, uint256 _staked) public {
+        staked[voter] = _staked;
+    }
+
+    function __stubInOffice(bool _inOffice) public {
+        inOffice = _inOffice;
     }
 }
